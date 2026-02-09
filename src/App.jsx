bebc8d9b915c1066d1b84477c26ed54a21656f0f
aryanrods <rodriguesaryan00@gmai.com>
2026-02-09 21:35:01 +1100

    feat add expense added in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
 import { useState } from "react";
+import AddUser from "./components/AddUser";
+import AddExpense from "./components/AddExpense";
 
 function App() {
   const [users, SetUsers] = useState([]);
 
-  function AddUser(name) {
+  function addUser(name) {
     const newUser = {
       name: name,
       balance: 0,
     };
+
+    SetUsers([...users, newUser]);
+  }
+
+  function AddExpense(payer, amount) {
+    const splitAmount = amount / users.length;
+
+    const newArray = users.map((user) => {
+      if (users.name === payer) {
+        return { ...users, balance: user.balance - splitamount + amount };
+      } else {
+        return { ...user, balance: balance - splitAmount };
+      }
+    });
   }
 
   return;
